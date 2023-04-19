9561ee99dc75d999ac9284c9cbfe06ca843afaab
MaagMichael <maagm@t-online.de>
2023-04-19 23:04:36 +0200

    mvp home

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,51 +20,69 @@ export default function Main() {
   return (
     <main>
       <section>
+        <div className="main_center">
+          <div
+            className="main_image"
+            style={{ backgroundImage: `url(${picture1})` }}
+          ></div>
+          <div className="main overlay">Bild 1</div>
+        </div>
         {/* https://upmostly.com/tutorials/react-background-image */}
-        <div
-          className="main_image"
-          style={{ backgroundImage: `url(${picture1})` }}
-        ></div>
-        <div className="main overlay">Bild 1</div>
 
-        <p>
-          <span>Platzhalter-Home 1</span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit
-          eum tenetur architecto odit possimus explicabo ea? Deleniti tempora
-          impedit modi autem error corrupti ut eveniet inventore possimus nulla
-          dicta architecto fuga pariatur obcaecati, iste doloremque totam
-          laudantium beatae consequatur magnam! Beatae dolorum non facilis sequi
-          numquam id architecto hic.
-        </p>
-        <Button />
+        <div className="main_text1">
+          <p>
+            <span>Platzhalter-Home 1</span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit
+            eum tenetur architecto odit possimus explicabo ea? Deleniti tempora
+            impedit modi autem error corrupti ut eveniet inventore possimus
+            nulla dicta architecto fuga pariatur obcaecati, iste doloremque
+            totam laudantium beatae consequatur magnam! Beatae dolorum non
+            facilis sequi numquam id architecto hic.
+          </p>
+          <Button />
+        </div>
       </section>
 
       <hr />
-      
+
       <section>
-        <div
-          className="main_image"
-          style={{ backgroundImage: `url(${picture2})` }}
-        ></div>
-        <div className="main overlay">Bild 2</div>
-        <h1>Anlässe</h1>
-        <p>
-          <span>Platzhalter-Home 2</span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit
-          eum tenetur architecto odit possimus explicabo ea? Deleniti tempora
-          impedit modi autem error corrupti ut eveniet inventore possimus nulla
-          dicta architecto fuga pariatur obcaecati, iste doloremque totam
-          laudantium beatae consequatur magnam! Beatae dolorum non facilis sequi
-          numquam id architecto hic.
-        </p>
-        <div
-          className="main_image"
-          style={{ backgroundImage: `url(${picture3})` }}
-        ></div>
-        <div className="main overlay">Bild 3</div>
+        <div className="main_event main_1">
+          <div className="main_center">
+            <div
+              className="main_image"
+              style={{ backgroundImage: `url(${picture2})` }}
+            ></div>
+            <div className="main overlay">Bild 2</div>
+          </div>
 
-        <Button />
+          <div className="main_center main_text1">
+            <h1>Anlässe</h1>
+            <p>
+              <span>Platzhalter-Home 2</span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+              impedit eum tenetur architecto odit possimus explicabo ea?
+              Deleniti tempora impedit modi autem error corrupti ut eveniet
+              inventore possimus nulla dicta architecto fuga pariatur obcaecati,
+              iste doloremque totam laudantium beatae consequatur magnam! Beatae
+              dolorum non facilis sequi numquam id architecto hic.
+            </p>
+          </div>
+        </div>
 
+        <div className="main_event ">
+          <div
+            className="main_image"
+            style={{ backgroundImage: `url(${picture3})` }}
+          ></div>
+          <div className="main overlay">Bild 3</div>
+
+          <Button />
+        </div>
+      </section>
+
+      <hr />
+
+      <section>
         <div className="main_video">
           <video
             controls
@@ -75,7 +93,7 @@ export default function Main() {
             onMouseOver={playMovie}
             onMouseOut={stopMovie}
           >
-            <source src= "images/video_nafas.mp4" type="video/mp4"/>
+            <source src="images/video_nafas.mp4" type="video/mp4" />
           </video>
           <div className="main overlay">Video 1</div>
         </div>
@@ -83,15 +101,17 @@ export default function Main() {
 
       <hr />
 
-      <section>
+      {/* <section>
         <h1>Statistik (Platzhalter Home 3)</h1>
       </section>
 
-      <hr />
+      <hr /> */}
 
-      <section>      
-        <Slider/>
-        <div className="main overlay">Bild(er) 4</div>
+      <section>
+        <div className="main_slider">
+          <Slider />
+          <div className="main overlay">Bild(er) 4</div>
+        </div>
       </section>
     </main>
   );
