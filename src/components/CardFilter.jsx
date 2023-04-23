002d3bdfaf7f4c352806e2a7b2cd46c5a372da0f
MaagMichael <maagm@t-online.de>
2023-04-23 21:31:02 +0200

    draft

--- src/components/CardFilter.jsx
+++ src/components/CardFilter.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 
+import Modal from "./CardModal";
+import useModal from "./useModal";
+
 import "./CardFilter.css";
 
 // read in database
 import Dishes from "../data.json";
 
 const CardFilter = (props) => {
-  
   // const package is reserved in strict mode
   const bundle = props.package;
   const cluster = props.category;
 
+  // using the custom hook
+  const { isShowing, toggle } = useModal();
+
+  // const infoDetail = (key) => {alert("You clicked dish ID " + key); };
   // const infoDetail = (key) => {alert("You clicked dish ID " + key); };
 
   return (
     <div className="card_section">
-        {Dishes &&
-          // https://upmostly.com/tutorials/react-filter-filtering-arrays-in-react-with-examples
-          
-          Dishes.filter((dish) => dish.package.includes(bundle))
-            .filter((dish) => dish.category.includes(cluster))
-            .map((dish) => {
-              return (
-                <div
-                  key={dish.id}
-                  className="card_container"
-                  // onClick={() => infoDetail(dish.id)}
-                >
-                  {/* images from URL */}
-                  <img src={dish.URL} className="card_image" alt="dish" />
-                  {/* text on overlay banner */}
-                  <div className="card_overlay">
-                    {dish.name} <br />( {dish.category} )
-                  </div>
+      {Dishes &&
+        // https://upmostly.com/tutorials/react-filter-filtering-arrays-in-react-with-examples
+
+        Dishes.filter((dish) => dish.package.includes(bundle))
+          .filter((dish) => dish.category.includes(cluster))
+          .map((dish) => {
+            return (
+              <div
+                key={dish.id}
+                className="card_container"
+                // onClick={() => infoDetail(dish.id)}
+                onClick={() => toggle()}
+                // onClick={toggle}
+              >
+                <Modal isShowing={isShowing} hide={toggle} id={dish.id} />
+
+                {/* images from URL */}
+                <img src={dish.URL} className="card_image" alt="dish" />
+                {/* text on overlay banner */}
+                <div className="card_overlay">
+                  {dish.name} <br />( {dish.category} )
                 </div>
-              );
-            })}
-      </div>
+              </div>
+            );
+          })}
+    </div>
   );
 };
 
---
+++ src/components/CardModal.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./CardModal.css";
+
+// Portals allow React components to render in another part of the DOM that
+//  is outside of their parent component.
+// Therefore, we can use a Portal to mount our Modal component to the end of the 
+// document.body element, rather than as a child of another component.
+// To do this in the code above, we specify two arguments to the createPortal
+//  function: the modal component we want to render and the location of 
+// where we want to append the component.
+
+const Modal = ({ isShowing, hide, id }) => isShowing ? ReactDOM.createPortal(
+  <React.Fragment>
+    <div className="modal-overlay"/>
+    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      <div className="modal">
+        <div className="modal-header">
+          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <p>
+          Hello, I'm the modal from dish number {id}.
+        </p>
+      </div>
+    </div>
+  </React.Fragment>, document.body
+) : null;
+
+export default Modal;
